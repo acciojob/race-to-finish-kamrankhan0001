18a36f4007dd9e70a544f8fb992f01433bf0c6d7
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-20 22:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ for (let i = 0; i < 5; i++) {
 	//number between 0 (inclusive) and 5 (exclusive).
 	// =>Adding 1 to the result of Math.floor(Math.random() * 5)
 	// 	gives a random number between 1 (inclusive) and 5 (inclusive).
-	// =>We then use Math.floor() to round down to the nearest integer
-  const randomTime = Math.random() * 5) + 1;
+	// =>We then use Math.floor() to round down to the nearest integer.
+  const randomTime = Math.floor(Math.random() * 5) + 1;
   const promise = new Promise((resolve) => {
 
     setTimeout(() => {
