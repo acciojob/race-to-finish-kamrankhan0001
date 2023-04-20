4f7408ec628a170e643d43f00d4bc873f362ebf2
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-20 21:18:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-var div= document.getElementById("value").value
-promise.any(){
-	return div;
-}
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(1), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(2), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(3), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(4), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(5), Math.floor(Math.random() * 5000) + 1000))
+];
+
+Promise.any(promises).then(result => {
+  document.getElementById("output").innerHTML = result;
+}).catch(error => {
+  console.log(error);
+});
+
 
