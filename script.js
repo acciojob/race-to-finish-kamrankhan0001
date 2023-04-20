f75ef5f61b349715c503111c0c0ab8d80016fff6
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-20 21:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+var div= document.getElementById("value").value
+promise.any(){
+	return div;
+}
+
